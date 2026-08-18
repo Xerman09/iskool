45e097e33e9d98a9895cfa7d581c9867750f675a
gustqt <gustcuti3@gmail.com>
2026-08-19 01:42:59 +0800

    Restore vendor directory and re-track it in git
    Prevents needing composer install to recover the private
    spondonit/service GitLab package, which end users of this
    codebase cannot re-download without vendor-owned credentials.

---
+++ vendor/spondonit/school-service/resources/lang/en/install.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    "welcome" => "Welcome",
+    "welcome_title" => "Welcome To eSkooly Pro",
+"admin_setup" => "Admin Setup",
+"done_msg" => "Installation done. You can now login.",
+"email" => "Email",
+"password" => "Password",
+"password_confirmation" => "Confirm Password",
+'with_demo_data' => 'Install With Demo Data',
+"welcome_description" => "Thank you for choosing eSkooly Pro for online school management. Please follow the steps to complete eSkooly Pro installation!",
+);
